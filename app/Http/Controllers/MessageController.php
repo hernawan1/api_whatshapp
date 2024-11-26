c5230b61a980693e4da89563ec35af86040beb0c
Hernawan Septiansyah <hernawanseptiansyah@gmail.com>
2024-11-26 09:34:31 +0700

    fix

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -35,12 +35,12 @@ class MessageController extends Controller
                 $validatedData['id_attachmemt'] = $attchmemt->id;
 
                 $createMessage = $message->create($validatedData);
-                $message->with(['attachmemt']);
+                $message->with(['attachmemt'])->get();
                 event(new MessageSent($message));
                 return $this->successResponse($createMessage);
             }
             $createMessage = $message->create($validatedData);
-            $message->with(['attachmemt']);
+            $message->with(['attachmemt'])->get();
             event(new MessageSent($message));
             return $this->successResponse($createMessage);
         }catch (Exception $e) {
