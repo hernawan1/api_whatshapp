9ee2c45b7f2094d8f8bd708d9c65e2e071d5d073
Hernawan Septiansyah <hernawanseptiansyah@gmail.com>
2024-11-26 10:24:51 +0700

    fix

--- app/Http/Requests/StoreMessage.php
+++ app/Http/Requests/StoreMessage.php
@@ -17,7 +17,8 @@ class StoreMessage extends BaseFormRequest
             'id_chatroom'       => 'required|string|exists:chatrooms,id',
             'id_user'           => 'nullable|string|exists:users,id',
             'id_attachmemt'     => 'nullable|string|exists:attachmemts,id',
-            'message'           => 'nullable|string'
+            'message'           => 'nullable|string',
+            'type_message'      => 'required|string|in:file,text,link'
         ];
     }
 }
