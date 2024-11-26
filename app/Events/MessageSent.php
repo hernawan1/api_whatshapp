dc4a205f7d93277696883a72ee64a16c083701b2
Hernawan Septiansyah <hernawanseptiansyah@gmail.com>
2024-11-26 10:02:29 +0700

    fix

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -23,7 +23,7 @@ class MessageSent implements ShouldBroadcastNow
 
     public function __construct(Message $message)
     {
-        $this->message = $message->orderBy('created_at', 'ASC')->first();
+        $this->message = $message->orderBy('created_at', 'DESC')->first();
     }
 
     public function broadcastOn()
